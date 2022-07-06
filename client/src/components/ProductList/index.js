0365c1f9c66566ca46fcabc81419a0244c578b8d
Sip <sipg789@gmail.com>
2022-07-06 15:57:06 -0500

    create success components

--- client/src/components/ProductList/index.js
+++ client/src/components/ProductList/index.js
@@ -1,54 +1,47 @@
 import React, { useEffect } from 'react';
-import { useQuery } from '@apollo/client';
+import ProductItem from '../ProductItem';
 import { useStoreContext } from '../../utils/GlobalState';
 import { UPDATE_PRODUCTS } from '../../utils/actions';
-import ProductItem from '../ProductItem';
+import { useQuery } from '@apollo/client';
 import { QUERY_PRODUCTS } from '../../utils/queries';
-import spinner from '../../assets/spinner.gif';
 import { idbPromise } from '../../utils/helpers';
+import spinner from '../../assets/spinner.gif';
 
 function ProductList() {
   const [state, dispatch] = useStoreContext();
 
-const { currentCategory } = state;
+  const { currentCategory } = state;
 
-const { loading, data } = useQuery(QUERY_PRODUCTS);
+  const { loading, data } = useQuery(QUERY_PRODUCTS);
 
-useEffect(() => {
-  // if theres data to be stored 
-  if (data) {
-    // lets store it in the global state object
-    dispatch({
-      type: UPDATE_PRODUCTS,
-      products: data.products
-    });
-
-
-    // but lets also take each product and save it to indexDB using the helper function 
-    data.products.forEach((product) => {
-      idbPromise('products', 'put', product);
-    });
-    // add else if to check if 'loading' is underfined in 'useQuery()' hook
-  } else if (!loading) {
-    // since were offline get all of the data from the products store
-    idbPromise('products', 'get').then((products) => {
-      // use retrieved data to set global state for offline browsing 
+  useEffect(() => {
+    if (data) {
       dispatch({
         type: UPDATE_PRODUCTS,
-        products: products
+        products: data.products,
       });
-    });
+      data.products.forEach((product) => {
+        idbPromise('products', 'put', product);
+      });
+    } else if (!loading) {
+      idbPromise('products', 'get').then((products) => {
+        dispatch({
+          type: UPDATE_PRODUCTS,
+          products: products,
+        });
+      });
+    }
+  }, [data, loading, dispatch]);
+
+  function filterProducts() {
+    if (!currentCategory) {
+      return state.products;
+    }
+
+    return state.products.filter(
+      (product) => product.category._id === currentCategory
+    );
   }
-
-}, [data, loading, dispatch]);
-
-function filterProducts() {
-  if (!currentCategory) {
-    return state.products;
-  }
-
-  return state.products.filter(product => product.category._id === currentCategory);
-}
 
   return (
     <div className="my-2">
